b24c8eff44900448fbd1e43a5ef157eac62e7084
ankit <ankit@moneytap.com>
2021-02-01 14:51:04 +0530

    Initial commit

---
+++ src/main/java/com/example/loanRestructre/LoanList.java
@@ -0,0 +1,15 @@
+package com.example.loanRestructre;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LoanList {
+    private String emi;
+    private String principal;
+    private String interest;
+    private String date;
+}
---
+++ src/test/java/com/example/loanRestructre/LoanRestructreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanRestructre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanRestructreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
